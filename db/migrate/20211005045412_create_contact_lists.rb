26d386374ff5ecdeb86fa9d947475648381701ac
Keenan Jones <keenanjones210@gmail.com>
2021-10-04 23:57:00 -0500

    Working on models

---
+++ db/migrate/20211005045412_create_contact_lists.rb
@@ -0,0 +1,12 @@
+class CreateContactLists < ActiveRecord::Migration[6.0]
+  def change
+    create_table :contact_lists do |t|
+      t.integer :contacts_awaiting
+      t.integer :contacts_made
+      t.integer :prospects_signed
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
